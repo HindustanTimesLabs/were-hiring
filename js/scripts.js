a72e747033b4f3351fc992fe496ca6a4f7d356ba
HarryStevens <harryjosephstevens@gmail.com>
2017-09-21 19:12:18 +0530

    Add image section

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 d3.csv("data/projects.csv", function(error, data){
 	if (error) throw error;
 
+	// shuffle it
+
 	data.forEach(function(project){
 
-		$(".projects-wrapper").append('<a href="' + project.url + '"><div class="project"><div class="project-title">' + project.headline + '</div><div class="project-image"><img src="' + project.image + '" /></div></div></a>')
+		$(".projects-wrapper").append('<a href="' + project.url + '"><div class="project" style="background-image:url(' + project.image + ')"><div class="project-title">' + project.headline + '</div></div></a>')
 		
 	});	
 })
